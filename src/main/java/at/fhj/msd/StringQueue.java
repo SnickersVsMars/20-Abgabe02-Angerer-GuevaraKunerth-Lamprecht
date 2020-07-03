f7068bdc75d91cfff16dead140900e5f14b85290
martin-gk <guevara.mrtn@gmail.com>
2020-07-03 12:21:45 +0200

    Debugged StringQueue.java, added comments at the corresponding lines to explain bug

--- src/main/java/at/fhj/msd/StringQueue.java
+++ src/main/java/at/fhj/msd/StringQueue.java
@@ -13,35 +13,41 @@ public class StringQueue implements IQueue<String> {
   private List<String> elements = new ArrayList<String>();
   private int maxSize = 5;
 
+//bug found: in constructor body both variables had the same name. changed second maxSize to maxsize
   public StringQueue(int maxsize) {
-    maxSize = maxSize;
+    maxSize = maxsize;
   }
 
+
+ //bug found: instead of != maxSize changed to < maxSize as our list has to be smaller in order to be able to add something.
+ //technically not a bug, but makes more sense logic wise
   @Override
   public boolean offer(String obj) {
-    if (elements.size() != maxSize)
+    if (elements.size() < maxSize)
       elements.add(obj);
     else
       return false;
 
     return true;
   }
-
+  
+//bug found: changed the if statement. size has to be bigger than 0 to be able to delete something
   @Override
   public String poll() {
     String element = peek();
 
-    if (elements.size() == 0) {
+    if (elements.size() != 0) {
       elements.remove(0);
     }
 
     return element;
   }
 
+  //bug found: removed "element = "";" otherwise if statement always false
   @Override
   public String remove() {
     String element = poll();
-    element = "";
+
     if (element == null)
       throw new NoSuchElementException("there's no element any more");
 
